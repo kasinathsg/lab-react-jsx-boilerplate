aded14d0e51242e6812d1560fabb7f39517f1dba
Kasinath S G <navaneeth.arunkumar@kalvium.community>
2022-12-22 15:44:56 +0530

    React

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
+
 import './App.css';
-import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
-
-function App() {
-  // code here
+function App(props) {
+  return (
+      <div className="lab">
+          <h1>Kalvium Gallery </h1>
+          <div className="image">
+              {props.imageData.map((elt) => {
+                  return <img src={elt.img} alt="elephant" key={elt.id} />;
+              })}
+          </div>
+      </div>
+  );
 }
 
 export default App;
--- src/AppClass.js
+++ src/AppClass.js
@@ -1,30 +1,20 @@
-import { Component } from "react";
-import "./App.css"
-import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
+  import { Component } from "react";
+  import "./App.css"
   
-  imageData = ()=>{
-    let data = [
-      {
-        id:1,
-        img:elephant
-      },
-      {
-        id:2,
-        img:elephant
-      },
-      {
-        id:3,
-        img:elephant
-      },
-      {
-        id:4,
-        img:elephant
-      }
-    ]
-    return data;
+  export default class AppClass extends Component{
+  
+    render(){
+      return (
+          <div className="lab">
+              <h1>Kalvium Gallery </h1>
+              <div className="image">
+                  {this.props.imageData.map((elt) => {
+                      return <img src={elt.img} alt="elephant" key={elt.id} />;
+                  })}
+              </div>
+          </div>
+      );
+    }
+    
   }
-
-  // code here
-}
